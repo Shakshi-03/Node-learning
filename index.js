10292bb5d55141c35907a2876f29d4807a50ecb0
Shakshi <kejriwalshakshi@gmail.com>
2025-02-19 14:01:47 +0545

    Added package.json, package-lock.json, and .gitignore

--- index.js
+++ index.js
@@ -13,4 +13,4 @@ console.log("i enjoy learning this series of node js")
 // working architecture of node js
 // 1.call stack(runs the main() function first by default and only when its task is finished node api code goes to call stack)
 // 2.node api(here comes the function which are inherited from the othe core's like C,C++,etc)
-// 3.callback queue
+// 3.callback queue'
