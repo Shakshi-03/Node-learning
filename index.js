4a841454b0034db3eddbbb24596c6ec93adfdef0
Shakshi <kejriwalshakshi@gmail.com>
2025-02-19 13:49:09 +0545

    updated form

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ setTimeout(() => {
 }, 0);
 
 console.log("i enjoy learning this series of node js")
+
+// working architecture of node js
+// 1.call stack(runs the main() function first by default and only when its task is finished node api code goes to call stack)
+// 2.node api(here comes the function which are inherited from the othe core's like C,C++,etc)
+// 3.callback queue
