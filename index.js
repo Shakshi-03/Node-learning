78694fd2b9302d2082199b0476eca350599f6496
Shakshi <kejriwalshakshi@gmail.com>
2025-02-19 14:15:45 +0545

    add express code

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
-console.log("hello this is shakshi kejriwal");
+// console.log("hello this is shakshi kejriwal");
 
-setTimeout(() => {
-  console.log("also i live in kathmandu");
-}, 2000);
+// setTimeout(() => {
+//   console.log("also i live in kathmandu");
+// }, 2000);
 
-setTimeout(() => {
-  console.log("i am from janakpur");
-}, 0);
+// setTimeout(() => {
+//   console.log("i am from janakpur");
+// }, 0);
 
-console.log("i enjoy learning this series of node js")
+// console.log("i enjoy learning this series of node js")
 
 // working architecture of node js
 // 1.call stack(runs the main() function first by default and only when its task is finished node api code goes to call stack)
 // 2.node api(here comes the function which are inherited from the othe core's like C,C++,etc)
 // 3.callback queue'
+
+
+const express=require('express');
+const app=express();
+
+app.get('',(res,resp)=>{
+  resp.send("welcome to home page");
+});
+
+app.get('/about',(res,resp)=>{
+  resp.send("welcome to about page");
+});
+
+app.get('/help',(res,resp)=>{
+  resp.send("welcome to help page");
+});
+
+app.listen(4000);
