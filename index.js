d4f308e2c44be19bc51ae0153e480562cce55707
Shakshi <kejriwalshakshi@gmail.com>
2025-02-19 13:36:07 +0545

    Working flow of node Js...call stack>>Node api>>callback queue

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("hello this is shakshi kejriwal");
+
+setTimeout(() => {
+  console.log("also i live in kathmandu");
+}, 2000);
+
+setTimeout(() => {
+  console.log("i am from janakpur");
+}, 0);
+
+console.log("i enjoy learning this series of node js")
