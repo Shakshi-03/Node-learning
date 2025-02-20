fa914557862e25a843149e18524ff7be9fd0f8d4
Shakshi <kejriwalshakshi@gmail.com>
2025-02-20 16:30:00 +0545

    routing with express

--- index.js
+++ index.js
@@ -1,33 +1,17 @@
-// console.log("hello this is shakshi kejriwal");
-
-// setTimeout(() => {
-//   console.log("also i live in kathmandu");
-// }, 2000);
-
-// setTimeout(() => {
-//   console.log("i am from janakpur");
-// }, 0);
-
-// console.log("i enjoy learning this series of node js")
-
-// working architecture of node js
-// 1.call stack(runs the main() function first by default and only when its task is finished node api code goes to call stack)
-// 2.node api(here comes the function which are inherited from the othe core's like C,C++,etc)
-// 3.callback queue'
-
-
 const express=require('express');
 const app=express();
 
-app.get('',(res,resp)=>{
-  resp.send("welcome to home page");
+app.get('',(req,resp)=>{
+  console.log("data sent by browser---->>",req.query.name);///>>routing with expressjs
+  resp.send("welcome"+ req.query.name);
 });
 
-app.get('/about',(res,resp)=>{
+app.get('/about',(req,resp)=>{
+ 
   resp.send("welcome to about page");
 });
 
-app.get('/help',(res,resp)=>{
+app.get('/help',(req,resp)=>{
   resp.send("welcome to help page");
 });
 
