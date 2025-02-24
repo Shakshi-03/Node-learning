722a6e0e159ef5e3285fdd8f9e699ffc73d1bc5c
Shakshi <kejriwalshakshi@gmail.com>
2025-02-24 18:09:40 +0545

    dynamic page with ejs

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const publicPath=path.join(__dirname,'public');
 
 // app.use(express.static(publicPath));
 
+app.set('view engine','ejs');
+
 app.get('',(_,resp)=>{
   resp.sendFile(`${publicPath}/index.html`)
 })
@@ -14,7 +16,23 @@ app.get('/about',(_,resp)=>{
   resp.sendFile(`${publicPath}/about.html`)
 })
 
+
+app.get('/profile',(_,resp)=>{
+  const user={
+    name:'Shakshi kejriwal',
+    email:'kejriwal@gmail',
+    city:'jnk',
+    skill:['php','c++','java','python']
+  }
+  resp.render('profile',{user});
+})
+app.get('/login',(_,resp)=>{
+  resp.render('login');
+})
+
 app.get('*',(_,resp)=>{
   resp.sendFile(`${publicPath}/404page.html`)/// make 404 page 
-})
+});
+
+
 app.listen(4000);
